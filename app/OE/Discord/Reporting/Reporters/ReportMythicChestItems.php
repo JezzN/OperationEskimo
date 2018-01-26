b306883db9de50d7981fd349c4938dc34d7c48df
Jeremy Norman <gbjezz@gmail.com>
2018-01-26 07:49:21 +0100

    Only reporting raiders.

--- app/OE/Discord/Reporting/Reporters/ReportMythicChestItems.php
+++ app/OE/Discord/Reporting/Reporters/ReportMythicChestItems.php
@@ -4,18 +4,40 @@ namespace App\OE\Discord\Reporting\Reporters;
 use App\OE\Discord\OperationEskimoDiscord;
 use App\OE\Discord\Reporting\AbstractDatabaseChangeReporter;
 use App\OE\Loot\LootDrop;
+use App\OE\OperationEskimo;
 use Carbon\Carbon;
 use Discord\Discord;
+use Illuminate\Database\DatabaseManager;
 
 class ReportMythicChestItems extends AbstractDatabaseChangeReporter
 {
+    /** @var OperationEskimo */
+    private $oe;
+
+    /**
+     * ReportMythicChestItems constructor.
+     *
+     * @param DatabaseManager $db
+     * @param OperationEskimo $oe
+     */
+    public function __construct(DatabaseManager $db, OperationEskimo $oe)
+    {
+        parent::__construct($db);
+        $this->oe = $oe;
+    }
+
     public function execute(Discord $discord)
     {
         $oeDiscord = OperationEskimoDiscord::forServer($discord);
 
         $items = $this->getNewRecords(LootDrop::where('context', 'challenge-mode-jackpot')->where('item_level', '>=', 930)->where('loot_time', '>', Carbon::now()->subDay())->orderBy('loot_time', 'asc'));
 
+        $raiderNames = $this->oe->raiders()->pluck('character_name');
+
         foreach( $items as $item ) {
+
+            if( ! $raiderNames->contains($item->character_name) ) continue;
+
             $oeDiscord->sendMessageToGeneralChat("**{$item->character_name}** looted **{$item->name} ({$item->item_level})** from their mythic chest!");
         }
     }
