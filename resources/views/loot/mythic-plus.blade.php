37172ab7b1ec7833b0c93362c492d9c705688c9f
Jeremy Norman <gbjezz@gmail.com>
2018-09-18 13:32:33 +0200

    Increased max records

--- resources/views/loot/mythic-plus.blade.php
+++ resources/views/loot/mythic-plus.blade.php
@@ -2,48 +2,48 @@
 
 @section('loot-content')
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-4">
             <h3>Mythic+ This Reset</h3>
             <table class="table table-striped"   >
                 <tr>
                     <th>Player</th>
-                    <th>2+</th>
-                    <th>5+</th>
-                    <th>10+</th>
-                    <th>15+</th>
                     <th>Total</th>
                 </tr>
                 @foreach( $completionsThisReset as $player => $activity )
                     <tr>
                         <td>{{ $player  }}</td>
-                        <td>{{ $activity['plus_2']  }}</td>
-                        <td>{{ $activity['plus_5']  }}</td>
-                        <td>{{ $activity['plus_10']  }}</td>
-                        <td>{{ $activity['plus_15']  }}</td>
                         <td>{{ $activity['total']  }}</td>
                     </tr>
                 @endforeach
             </table>
         </div>
 
-        <div class="col-md-6">
+        <div class="col-md-4">
             <h3>Mythic+ This Month</h3>
             <table class="table table-striped"   >
                 <tr>
                     <th>Player</th>
-                    <th>2+</th>
-                    <th>5+</th>
-                    <th>10+</th>
-                    <th>15+</th>
                     <th>Total</th>
                 </tr>
                 @foreach( $completionsThisMonth as $player => $activity )
                     <tr>
                         <td>{{ $player  }}</td>
-                        <td>{{ $activity['plus_2']  }}</td>
-                        <td>{{ $activity['plus_5']  }}</td>
-                        <td>{{ $activity['plus_10']  }}</td>
-                        <td>{{ $activity['plus_15']  }}</td>
+                        <td>{{ $activity['total']  }}</td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+
+        <div class="col-md-4">
+            <h3>Mythic+ All Time</h3>
+            <table class="table table-striped"   >
+                <tr>
+                    <th>Player</th>
+                    <th>Total</th>
+                </tr>
+                @foreach( $completionsAllTime as $player => $activity )
+                    <tr>
+                        <td>{{ $player  }}</td>
                         <td>{{ $activity['total']  }}</td>
                     </tr>
                 @endforeach
@@ -51,29 +51,4 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <h3>Mythic+ All Time (since 09/09/2018)</h3>
-            <table class="table table-striped"   >
-                <tr>
-                    <th>Player</th>
-                    <th>2+</th>
-                    <th>5+</th>
-                    <th>10+</th>
-                    <th>15+</th>
-                    <th>Total</th>
-                </tr>
-                @foreach( $completionsAllTime as $player => $activity )
-                    <tr>
-                        <td>{{ $player  }}</td>
-                        <td>{{ $activity['plus_2']  }}</td>
-                        <td>{{ $activity['plus_5']  }}</td>
-                        <td>{{ $activity['plus_10']  }}</td>
-                        <td>{{ $activity['plus_15']  }}</td>
-                        <td>{{ $activity['total']  }}</td>
-                    </tr>
-                @endforeach
-            </table>
-        </div>
-    </div>
 @stop
