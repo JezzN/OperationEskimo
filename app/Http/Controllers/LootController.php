7fb6b0e860837b16ffddd70c1e0903d934a4b514
jeremy.norman <jeremy.norman@wearespindle.com>
2018-10-17 08:54:25 +0200

    Updated using leaderboards

--- app/Http/Controllers/LootController.php
+++ app/Http/Controllers/LootController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\OE\Forum\Link;
 use App\OE\Loot\LootDrop;
 use App\OE\Loot\LootStats;
+use App\OE\WoW\MythicLeaderboard;
 use App\OE\WoW\MythicPlusCompletion;
 use Carbon\Carbon;
 use Illuminate\Cache\Repository;
@@ -42,9 +43,9 @@ class LootController extends Controller
 
     public function mythicPlus()
     {
-        $completionsThisReset = $this->flattenCompletions(MythicPlusCompletion::whereBetween('created_at', $this->lootStats->thisWeek())->where('is_initial_recording', false)->get());
-        $completionsThisMonth =  $this->flattenCompletions(MythicPlusCompletion::whereBetween('created_at', [Carbon::now()->subMonth(), Carbon::now()])->where('is_initial_recording', false)->get());
-        $completionsAllTime =  $this->flattenCompletions(MythicPlusCompletion::where('is_initial_recording', false)->get());
+        $completionsThisReset = $this->flattenCompletions(MythicLeaderboard::whereBetween('completed_at', $this->lootStats->thisWeek())->get());
+        $completionsThisMonth = $this->flattenCompletions(MythicLeaderboard::whereBetween('completed_at', [Carbon::now()->subMonth(), Carbon::now()])->get());
+        $completionsAllTime =  $this->flattenCompletions(MythicLeaderboard::whereNotNull('completed_at')->get());
 
         return view('loot.mythic-plus')
             ->with('completionsThisReset', $completionsThisReset)
@@ -59,23 +60,10 @@ class LootController extends Controller
             if ($completion->is_initial_recording) continue;
             if (!array_key_exists($completion->character_name, $result)) {
                 $result[$completion->character_name] = [];
-                $result[$completion->character_name]['plus_2'] = 0;
-                $result[$completion->character_name]['plus_5'] = 0;
-                $result[$completion->character_name]['plus_10'] = 0;
-                $result[$completion->character_name]['plus_15'] = 0;
+                $result[$completion->character_name]['total'] = 0;
             }
 
-            $result[$completion->character_name]['plus_2'] += $completion->plus_2;
-            $result[$completion->character_name]['plus_5'] += $completion->plus_5;
-            $result[$completion->character_name]['plus_10'] += $completion->plus_10;
-            $result[$completion->character_name]['plus_15'] += $completion->plus_15;
-        }
-
-        foreach ($result as $i => $r) {
-            $result[$i]['total'] = $result[$i]['plus_2'];
-            $result[$i]['plus_2'] = ($result[$i]['plus_2'] - $result[$i]['plus_15'] - $result[$i]['plus_10'] - $result[$i]['plus_5']);
-            $result[$i]['plus_5'] = ($result[$i]['plus_5'] - $result[$i]['plus_15'] - $result[$i]['plus_10']);
-            $result[$i]['plus_10'] = ($result[$i]['plus_10'] - $result[$i]['plus_15']);
+            $result[$completion->character_name]['total']++;
         }
 
         return collect($result)->sortByDesc('total');
@@ -88,41 +76,9 @@ class LootController extends Controller
             ->with('raidersWithoutCache', $this->lootStats->withoutCacheReward());
     }
 
-    public function legendary()
-    {
-        $drops = LootDrop::orderBy('loot_time', 'desc')->legendary()->paginate(15);
-
-        $legendaries = LootDrop::legendary()->orderBy('loot_time', 'asc')->get();
-
-        $stats = [];
-
-        foreach( $legendaries as $drop ) {
-            $date = new Carbon($drop->loot_time);
-            $weekOfYear = $date->weekOfYear >= 10 ? $date->weekOfYear : "0{$date->weekOfYear}";
-            $date = "{$date->year}W{$weekOfYear}";
-
-            if( !isset($stats[$date]) ) $stats[$date] = 0;
-
-            $stats[$date]++;
-        }
-
-        $stats2 = [];
-
-        foreach( $stats as $year => $stat ) {
-            $stats2[] = [
-              'week' => (new Carbon($year))->format('Y-m-d'),
-                'count' => $stat
-            ];
-        }
-
-        return view('loot.legendary')
-            ->withDrops($drops)
-            ->withStats($stats2);
-    }
-
     public function mythicRaid()
     {
-        $drops = LootDrop::orderBy('loot_time', 'desc')->where('context', '=', 'raid-mythic')->where('item_level', '>=', 880)->paginate(40);
+        $drops = LootDrop::orderBy('loot_time', 'desc')->where('context', '=', 'raid-mythic')->where('item_level', '>=', 300)->paginate(40);
 
         return view('loot.mythic-raids')
             ->withDrops($drops);
